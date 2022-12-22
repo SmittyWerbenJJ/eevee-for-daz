777be7fde7682dfb783fd6a0407f467cc91d4ef6
Smitty Werben <smittywerbenjj.1@gmail.com>
2022-12-22 17:44:19 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,65 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import importlib
+from pathlib import Path
+
+bl_info = {
+    "name": "Eevee for Daz",
+    "author": "SmittyWerbenHD",
+    "description": "Fast eevee material setup for daz characters",
+    "blender": (3, 3, 0),
+    "version": (0, 0, 1),
+    "location": "",
+    "warning": "",
+    "category": "Generic"
+}
+
+# ---------------------------------------------
+# auto-load modules from sibling files
+# import modules in this directory
+# ---------------------------------------------
+moduleNames = []
+ignoredModules = [
+    Path(__file__).name,
+    "__init__.py",
+    "easybpy.py"
+]
+for file in Path(__file__).parent.glob("*.py"):
+    globfile = Path(file)
+    if globfile.name in ignoredModules:
+        continue
+    moduleNames.append(file.stem)
+
+# ui module has to be loaded first
+moduleNames.insert(0, moduleNames.pop(moduleNames.index("ui")))
+
+anchor = Path(__file__).parent.name
+modules = []
+for moduleName in moduleNames:
+    mod = importlib.import_module("." + moduleName, anchor)
+    modules.append(mod)
+
+# ---------------------------------------------
+# Blender Register
+# ---------------------------------------------
+
+
+def register():
+    for module in modules:
+        module.register()
+
+
+def unregister():
+    for module in reversed(modules):
+        module.unregister()
---
+++ ui.py
@@ -0,0 +1,59 @@
+import bpy
+import easybpy
+import bpy.utils.previews
+import os
+
+
+DAZ_ICON_NAME = "daz_icon"
+PCOL_MAIN = "main"
+ICON_DAZ = "icon_daz"
+preview_collections = {}
+
+
+class SMITTY_MT_EeveeForDaz(bpy.types.Menu):
+    """Creates a Panel in the Object properties window"""
+
+    bl_label = "Convert Materials for Daz Characters"
+    bl_idname = "SMITTY_MT_EeveeForDaz"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.separator()
+        layout.operator("smitty.eevee_for_daz")
+
+
+def eeveeForDaz_menu_functions(self, context):
+    layout = self.layout
+    my_icon = preview_collections[PCOL_MAIN][ICON_DAZ]
+    layout.menu(
+        SMITTY_MT_EeveeForDaz.bl_idname,
+        icon_value=my_icon.icon_id
+    )
+
+
+def register_icons():
+    my_icons_dir = os.path.join(os.path.dirname(__file__), "icons")
+    pcoll = bpy.utils.previews.new()
+    pcoll.load(ICON_DAZ, os.path.join(my_icons_dir, "daz_icon.png"), 'IMAGE')
+
+    preview_collections[PCOL_MAIN] = pcoll
+
+
+def unregister_icons():
+    for pcoll in preview_collections.values():
+        bpy.utils.previews.remove(pcoll)
+    preview_collections.clear()
+
+
+def register():
+    register_icons()
+    bpy.utils.register_class(SMITTY_MT_EeveeForDaz)
+    bpy.types.MATERIAL_MT_context_menu.append(eeveeForDaz_menu_functions)
+    # unregister_icons()
+    # bpy.utils.unregister_class(SMITTY_MT_EeveeForDaz)
+
+
+def unregister():
+    unregister_icons()
+    bpy.utils.unregister_class(SMITTY_MT_EeveeForDaz)
+    bpy.types.MATERIAL_MT_context_menu.remove(eeveeForDaz_menu_functions)
